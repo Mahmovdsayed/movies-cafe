8bb9d322b0efbfadea5f8414536a9951e7bacc61
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-05-25 09:32:37 +0300

    Update Login.tsx

--- Components/auth/login/Login.tsx
+++ Components/auth/login/Login.tsx
@@ -89,86 +89,93 @@ const Login = ({ }: IProps) => {
         onSubmit: submitRegister,
     });
     return <>
-        <div className="container mx-auto text-center px-4">
-            <h1 className="my-4 text-2xl md:text-3xl font-semibold">Login Now</h1>
-            <div className="">
-                <form onSubmit={formik.handleSubmit}>
-
-                    <Input
-                        className=" my-2"
-                        onBlur={formik.handleBlur}
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        startContent={<MdEmail />}
-                        label="Email"
-                        type="email"
-                        placeholder="enter your email"
-                        id="email"
-                        name="email"
-                    />
-                    {formik.errors.email && formik.touched.email ? (
-                        <div className="text-red-400 text-tiny p-1 ">
-                            {formik.errors.email}
-                        </div>
-                    ) : (
-                        ""
-                    )}
-
-
-                    <Input
-                        className=" my-2"
-                        onBlur={formik.handleBlur}
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        label="Password"
-                        type="password"
-                        startContent={<RiLockPasswordFill />}
-                        placeholder="Password"
-                        id="password"
-                        name="password"
-                    />
-                    {formik.errors.password && formik.touched.password ? (
-                        <div className="text-red-400 text-tiny p-1 ">
-                            {formik.errors.password}
-                        </div>
-                    ) : (
-                        ""
-                    )}
-
-                    {isLoading ? (
-                        <button
-                            className="mt-3" type="button">
-                            <Bars
-                                height="20"
-                                width="100"
-                                color="#fff"
-                                ariaLabel="bars-loading"
-                                wrapperStyle={{}}
-                                wrapperClass=""
-                                visible={true}
-                            />
-                        </button>
-                    ) : (
-                        <Button
-
-
-                            size="sm"
-                            startContent={<IoLogIn />}
-                            type="submit"
-                            className="mt-3 bg-indigo-500 text-white p-2 text-sm rounded-lg w-full lg:w-min"
-                        >
-                            Login Now
-                        </Button>
-                    )}
-                    <Link
-                        className=" mt-3 block text-sm"
-                        href={'/register'}
-                    >
-                        Don't have an account?  <span className="font-bold text-pink-400">Sign up</span>
-                    </Link>
-                </form>
+        <div className="container mx-auto md:flex md:items-center md:justify-center my-6 p-6">
+            <div className=" hidden md:flex w-1/2 ">
+                <div className="relative">
+                    <Image translate="yes" isBlurred className="w-3/4 m-auto object-cover object-center" removeWrapper src="https://image.tmdb.org/t/p/original/5weKu49pzJCt06OPpjvT80efnQj.jpg" />
+                   
+                </div>
             </div>
+            <div className="md:w-1/2 p-4 text-center">
+                <h1 className="my-4 text-2xl md:text-3xl font-semibold">Login Now</h1>
+                <div className="">
+                    <form onSubmit={formik.handleSubmit}>
 
+                        <Input
+                            className=" my-2"
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            startContent={<MdEmail />}
+                            label="Email"
+                            type="email"
+                            placeholder="enter your email"
+                            id="email"
+                            name="email"
+                        />
+                        {formik.errors.email && formik.touched.email ? (
+                            <div className="text-red-400 text-tiny p-1 ">
+                                {formik.errors.email}
+                            </div>
+                        ) : (
+                            ""
+                        )}
+
+
+                        <Input
+                            className=" my-2"
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            label="Password"
+                            type="password"
+                            startContent={<RiLockPasswordFill />}
+                            placeholder="Password"
+                            id="password"
+                            name="password"
+                        />
+                        {formik.errors.password && formik.touched.password ? (
+                            <div className="text-red-400 text-tiny p-1 ">
+                                {formik.errors.password}
+                            </div>
+                        ) : (
+                            ""
+                        )}
+
+                        {isLoading ? (
+                            <button
+                                className="mt-3" type="button">
+                                <Bars
+                                    height="20"
+                                    width="100"
+                                    color="#fff"
+                                    ariaLabel="bars-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                />
+                            </button>
+                        ) : (
+                            <Button
+
+
+                                size="sm"
+                                startContent={<IoLogIn />}
+                                type="submit"
+                                className="mt-3 dark:bg-white bg-black text-white font-medium dark:text-black p-2 text-sm rounded-lg w-full "
+                            >
+                                Login Now
+                            </Button>
+                        )}
+                        <Link
+                            className=" mt-3 block text-sm"
+                            href={'/register'}
+                        >
+                            Don't have an account?  <span className="font-bold text-pink-400">Sign up</span>
+                        </Link>
+                    </form>
+                </div>
+            </div>
         </div>
     </>;
 };
