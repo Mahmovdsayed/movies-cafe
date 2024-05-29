cdc64324c065b63552494b5a4e78f4b92e81f575
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-05-29 13:49:46 +0300

    Update page.tsx

--- app/movies/[slug]/page.tsx
+++ app/movies/[slug]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata({
     description: data.overview,
     keywords: [data.title],
     openGraph: {
-      images: [`https://image.tmdb.org/t/p/original${data.poster_path}`],
+      images: [`https://image.tmdb.org/t/p/original${data.backdrop_path}`],
       title: data.title,
       description: data.overview,
     },
@@ -34,7 +34,7 @@ export async function generateMetadata({
       creator: '@mahmovdsayed',
       title: data.title,
       description: data.overview,
-      images: [`https://image.tmdb.org/t/p/original${data.poster_path}`],
+      images: [`https://image.tmdb.org/t/p/original${data.backdrop_path}`],
     },
     robots: {
       index: false,
