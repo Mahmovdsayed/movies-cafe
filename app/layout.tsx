456cef8a3b984b1d54558ba798ff5bcd570e3b23
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-06 03:49:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata , Viewport } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../Providers/Providers";
@@ -10,6 +10,10 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from "@vercel/analytics/react";
 
 const inter = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], });
+export const viewport: Viewport = {
+  themeColor:"#0000FF",
+  colorScheme:"dark"
+}
 export const metadata: Metadata = {
   metadataBase: new URL('https://moviescafe.vercel.app'),
 
